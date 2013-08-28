201f1e705c5f98dbd60ebf502e33d48f0bc7d11a
kyung yeol kim <chitacan@gmail.com>
2013-08-28 23:28:05 +0900

    fix broken api when startup.

--- app/background.js
+++ app/background.js
@@ -1,3 +1,3 @@
-chrome.experimental.app.onLaunched.addListener(function() {
-  chrome.appWindow.create("static/index.html", {width:1164, height: 825});
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create("static/index.html", {width:1164, height: 825});
 });
